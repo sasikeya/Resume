6e9cca9e5359c5c04b357b2069eb6937e1e5568b
czhfight <czhfight@outlook.com>
2016-11-15 22:35:53 +0800

    个人简历

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('.preactivated').bind('inview', function () {
+    $(this).addClass("activated");
+});
+$('.progress-bar').bind('inview', function () {
+    $(this).addClass("activated");
+});
+
+
+
